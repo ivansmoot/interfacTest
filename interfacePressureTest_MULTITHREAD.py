772540dd19358a88f993e5bf5d4ec5efaf7f1b32
ivansmoot <ivansmoot@163.com>
2020-09-28 10:29:58 +0800

    :art:增加注释，优化格式

--- interfacePressureTest_MULTITHREAD.py
+++ interfacePressureTest_MULTITHREAD.py
@@ -9,46 +9,43 @@
 import requests
 import json
 import time
-from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
 
 
 url = 'http://127.0.0.1:6565/lottery'
 
-timeToRun = 10000
-prize_stuff = ['RTX3080', '三星980 pro', '猫头鹰D15S', '100元现金奖励', '200元代金券', '10000元余额宝体验基金']
-prize_num = [0] * len(prize_stuff)
+timeToRun = 10000  # 循环次数
+prize_stuff = ['RTX3080', '三星980 pro', '猫头鹰D15S', '100元现金奖励', '200元代金券', '10000元余额宝体验基金']  # 奖励list，顺序无所谓
+prize_num = [0] * len(prize_stuff)  # 创建一个和奖励list同样长度的list，初始化为0，用来记录每个奖励的次数
 
 
-def conn(add_prize):
-    res = requests.get(url, headers={'Connection': 'close'}, timeout=5)
-    res.encoding = 'utf-8'
-    js = json.loads(res.text)
-    for j in range(len(prize_stuff)):
+def conn(add_prize):  # 把网络请求提出来放到一个方法里，参数为需要修改的list
+    res = requests.get(url, headers={'Connection': 'close'}, timeout=5)  # 需要设置headers为connection close，否则大量请求会直接失败
+    res.encoding = 'utf-8'  # requests返回的结果需要编码，这里比较坑
+    js = json.loads(res.text)  # 转json
+
+    for j in range(len(prize_stuff)):  # 查找本次请求的返回值是奖励列表的哪一个，找到了就给prize_num同样位置的值+1
         if js['prize']['stuff'] == prize_stuff[j]:
-            add_prize[j] += 1
+            prize_num[j] += 1
 
 
 if __name__ == '__main__':
     startTime = time.time()
-    # for i in range(timeToRun):
-    #     conn()
-    # pool = Pool()
-    # for i in range(timeToRun):
-    #     pool.apply_async(conn, (prize_num,))
-    # pool.close()
-    # pool.join()
-    with ThreadPoolExecutor(max_workers=6) as pool:
+
+    with ThreadPoolExecutor(max_workers=6) as pool:  # 创建一个最大线程数为6的线程池，具体几个可以多试试
         for i in range(timeToRun):
-            pool.submit(conn, prize_num)
-    for i in range(len(prize_stuff)):
+            pool.submit(conn, prize_num)  # 将request提交给线程池
+
+    for i in range(len(prize_stuff)):  # 后续的统计
         print(prize_stuff[i], end="")
         print("返回了", end="")
         print(prize_num[i], end="")
         print("次，概率为:", end="")
-        Probability = round(prize_num[i] / timeToRun * 100, 3)
+        Probability = round(prize_num[i] / timeToRun * 100, 3)  # round函数保留三位小数
         print(Probability, end="")
         print("%")
+
     endTime = time.time()
+
     print("请求共耗时")
     print(endTime - startTime)
