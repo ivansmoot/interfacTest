622c48025ceb22d4b8433416c2fec03a04790218
ivansmoot <ivansmoot@163.com>
2020-09-28 10:14:43 +0800

    :art:主要逻辑写进__name__判断里

--- interfacePressureTest_NORMAL.py
+++ interfacePressureTest_NORMAL.py
@@ -17,28 +17,28 @@ prize_stuff = ['RTX3080', '三星980 pro', '猫头鹰D15S', '100元现金奖励'
 prize_num = [0] * len(prize_stuff)  # 创建一个和奖励list同样长度的list，初始化为0，用来记录每个奖励的次数
 
 
-startTime = time.time()
+if __name__ == "__main__":
+    startTime = time.time()
 
-for i in range(timeToRun):  # 循环请求该url
-    res = requests.get(url, headers={'Connection': 'close'}, timeout=5)  # 需要设置headers为connection close，否则大量请求会直接失败
-    res.encoding = 'utf-8'  # requests返回的结果需要编码，这里比较坑
-    js = json.loads(res.text)  # 转json
+    for i in range(timeToRun):  # 循环请求该url
+        res = requests.get(url, headers={'Connection': 'close'}, timeout=5)  # 需要设置headers为connection close，否则大量请求会直接失败
+        res.encoding = 'utf-8'  # requests返回的结果需要编码，这里比较坑
+        js = json.loads(res.text)  # 转json
 
-    for j in range(len(prize_stuff)):  # 查找本次请求的返回值是奖励列表的哪一个，找到了就给prize_num同样位置的值+1
-        if js['prize']['stuff'] == prize_stuff[j]:
-            prize_num[j] += 1
+        for j in range(len(prize_stuff)):  # 查找本次请求的返回值是奖励列表的哪一个，找到了就给prize_num同样位置的值+1
+            if js['prize']['stuff'] == prize_stuff[j]:
+                prize_num[j] += 1
 
+    for i in range(len(prize_stuff)):  # 后续的统计
+        print(prize_stuff[i], end="")
+        print("返回了", end="")
+        print(prize_num[i], end="")
+        print("次，概率为:", end="")
+        Probability = round(prize_num[i] / timeToRun * 100, 3)  # round函数保留三位小数
+        print(Probability, end="")
+        print("%")
 
-for i in range(len(prize_stuff)):  # 后续的统计
-    print(prize_stuff[i], end="")
-    print("返回了", end="")
-    print(prize_num[i], end="")
-    print("次，概率为:", end="")
-    Probability = round(prize_num[i] / timeToRun * 100, 3)  # round函数保留三位小数
-    print(Probability, end="")
-    print("%")
+    endTime = time.time()
 
-endTime = time.time()
-
-print("请求共耗时")
-print(endTime - startTime)
+    print("请求共耗时")
+    print(endTime - startTime)
