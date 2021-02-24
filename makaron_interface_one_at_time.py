1cacc6e6a0c890955567ac53cf42b3f8253a1002
ivansmoot <ivansmoot@163.com>
2021-02-24 19:25:04 +0800

    优化马卡龙接口测试代码

--- makaron_interface_one_at_time.py
+++ makaron_interface_one_at_time.py
@@ -1,15 +1,26 @@
+import traceback
+
 import requests
 import json
 import time
 from concurrent.futures import ThreadPoolExecutor
 
-url = f"https://appdev.api.versa-ai.com/menu/editing?appSource=web&appVersion=4.5.9&appkey=test&clientType=app" \
-      f"&countryCode=CN&deviceId=8a1449de224a3cc0&imei=&lang=zh-cn&mobileType=SM-G9650&osType=ANDROID&osVersion=10" \
-      f"&sign=47904D4FC34A1B0380AACC9B11A0BFF1&timestamp=1613804392458&uid=&userToken="
+url = []
+url.append(f"https://appdev.api.versa-ai.com/menu/editing?appSource=web&appVersion=4.5.9&appkey=test&clientType=app"
+           f"&countryCode=CN&deviceId=8a1449de224a3cc0&imei=&lang=zh-cn&mobileType=SM-G9650&osType=ANDROID&osVersion=10"
+           f"&sign=47904D4FC34A1B0380AACC9B11A0BFF1&timestamp=1613804392458&uid=&userToken=")
+# url.append(f"https://appdev.api.versa-ai.com/launch/ads?appSource=web&appVersion=5.0.0&appkey=test"
+#            f"&clientType=app&countryCode=CN&deviceId=182b77e08b8dbcff&imei=&lang=zh-cn&mobileType=OPPO_R11s"
+#            f"&osType=ANDROID&osVersion=8.1.0&sign=02C59B1340B4E37270D9451A846FB634&timestamp=1613976889862&uid="
+#            f"&userToken=")
+url.append(f"https://appdev.api.versa-ai.com/template?appSource=web&appVersion=5.0.0&appkey=test&clientType=app"
+           f"&countryCode=CN&deviceId=182b77e08b8dbcff&imei=&lang=zh-cn&mobileType=OPPO_R11s"
+           f"&osType=ANDROID&osVersion=8.1.0&sign=8974BC8E68CDF3AC38C1DA5460909940&timestamp=1613976891893"
+           f"&uid=&userToken=")
 
 time_to_Run = 10000  # 循环次数
 success_num_requests = [0] * 1  # 成功次数
-max_thread_num = 200  # 线程数
+max_thread_num = 60  # 线程数
 defeat_num_requests = [0] * 1  # 返回值非200的次数
 avg_time_each_successful_request = [0] * 1  # 成功的请求耗时
 
@@ -17,51 +28,65 @@ avg_time_each_successful_request = [0] * 1  # 成功的请求耗时
 def try_catch_conn(uri, code_not_200):
     response = ""
     try:
+        s = requests.session()
+        s.keep_alive = False  # 关闭多余连接
         # 需要设置headers为connection close，否则大量请求会直接失败
-        response = requests.get(uri, headers={'Connection': 'close'}, timeout=5)
+        response = requests.get(uri, headers={'Connection': 'close'}, timeout=(5, 5))
+        print(response.elapsed)
     except requests.exceptions.ConnectionError:
         code_not_200[0] += 1
+        traceback.print_exc()
         print("ConnectionError失败了")
     except requests.exceptions.HTTPError:
         code_not_200[0] += 1
+        traceback.print_exc()
         print("HTTPError失败了")
     except requests.exceptions.ReadTimeout:
         code_not_200[0] += 1
+        traceback.print_exc()
         print("ReadTimeout失败了")
     except requests.exceptions.RetryError:
         code_not_200[0] += 1
+        traceback.print_exc()
         print("RetryError失败了")
     except requests.exceptions.StreamConsumedError:
         code_not_200[0] += 1
+        traceback.print_exc()
         print("StreamConsumedError失败了")
     except Exception:
         code_not_200[0] += 1
+        traceback.print_exc()
         print("Exception失败了")
     return response
 
 
 def conn(response_code_is_200, response_code_not_200, request_time_if_successfully):
-    # res = ""  # 保存请求结果
-    start_time = time.time()
-    res = try_catch_conn(url, response_code_not_200)
-    end_time = time.time()
-    request_time = end_time - start_time  # 单次请求耗时
-    res.encoding = 'utf-8'  # requests返回的结果需要编码，这里比较坑
-    if str(res) == "<Response [200]>":
-        response_code_is_200[0] += 1
-        request_time_if_successfully[0] += request_time
-        print("返回码为200次数:" + str(response_code_is_200[0]))
-    else:
-        response_code_not_200[0] += 1
-        print("错误码：" + str(res))
-    js = json.loads(res.text)  # 转json
-    # print(js)
+    whole_start_time = time.time()
+    for i in range(len(url)):
+        start_time = time.time()
+        res = try_catch_conn(url[i], response_code_not_200)
+        end_time = time.time()
+        request_time = end_time - start_time  # 单次请求耗时
+        print("单次url请求耗时:" + str(request_time))
+        res.encoding = 'utf-8'  # requests返回的结果需要编码，这里比较坑
+        if str(res) == "<Response [200]>":
+            response_code_is_200[0] += 1
+            request_time_if_successfully[0] += request_time
+            print("返回码为200次数:" + str(response_code_is_200[0]))
+        else:
+            response_code_not_200[0] += 1
+            print("错误码：" + str(res))
+        js = json.loads(res.text)  # 转json
+        # print(js)
+    whole_end_time = time.time()
+    print("一次任务执行耗时:" + str(whole_end_time - whole_start_time))
 
 
 if __name__ == '__main__':
     startTime = time.time()
     with ThreadPoolExecutor(max_workers=max_thread_num) as pool:  # 创建一个最大线程数为6的线程池，具体几个可以多试试
-        for _ in range(time_to_Run):
+        # for _ in range(time_to_Run):
+        while(True):
             # 将request提交给线程池
             pool.submit(conn, success_num_requests, defeat_num_requests, avg_time_each_successful_request)
     endTime = time.time()
--- makaron_interface_one_at_time_multy_process.py
+++ makaron_interface_one_at_time_multy_process.py
@@ -15,12 +15,12 @@ url = f"https://appdev.api.versa-ai.com/menu/editing?appSource=web&appVersion=4.
       f"&countryCode=CN&deviceId=8a1449de224a3cc0&imei=&lang=zh-cn&mobileType=SM-G9650&osType=ANDROID&osVersion=10" \
       f"&sign=47904D4FC34A1B0380AACC9B11A0BFF1&timestamp=1613804392458&uid=&userToken="
 
-time_to_Run = 100  # 循环次数
+time_to_Run = 10000  # 循环次数
 success_num_requests = [0] * 1  # 成功次数
 max_thread_num = 200  # 线程数
 defeat_num_requests = [0] * 1  # 返回值非200的次数
 avg_time_each_successful_request = [0] * 1  # 成功的请求耗时
-processes_num = 5  # 进程数
+processes_num = 10  # 进程数
 
 
 # 定义一个函数，准备作为新进程的 target 参数
@@ -52,20 +52,21 @@ def try_catch_conn(uri, code_not_200):
 
 
 def conn(response_code_is_200, response_code_not_200, request_time_if_successfully):
-    this_pid = str(os.getpid())
-    # res = ""  # 保存请求结果
-    start_time = time.time()
-    res = try_catch_conn(url, response_code_not_200)
-    end_time = time.time()
-    request_time = end_time - start_time  # 单次请求耗时
-    res.encoding = 'utf-8'  # requests返回的结果需要编码，这里比较坑
-    if str(res) == "<Response [200]>":
-        response_code_is_200[0] += 1
-        request_time_if_successfully[0] += request_time
-        print("[" + this_pid + "]返回码为200次数:" + str(response_code_is_200[0]))
-    else:
-        response_code_not_200[0] += 1
-        print("[" + this_pid + "]错误码：" + str(res))
+    for _ in range(int(time_to_Run / processes_num)):
+        this_pid = str(os.getpid())
+        # res = ""  # 保存请求结果
+        start_time = time.time()
+        res = try_catch_conn(url, response_code_not_200)
+        end_time = time.time()
+        request_time = end_time - start_time  # 单次请求耗时
+        res.encoding = 'utf-8'  # requests返回的结果需要编码，这里比较坑
+        if str(res) == "<Response [200]>":
+            response_code_is_200[0] += 1
+            request_time_if_successfully[0] += request_time
+            print("[" + this_pid + "]返回码为200次数:" + str(response_code_is_200[0]))
+        else:
+            response_code_not_200[0] += 1
+            print("[" + this_pid + "]错误码：" + str(res))
 
 
 if __name__ == '__main__':
