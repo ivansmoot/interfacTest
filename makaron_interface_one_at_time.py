ca563f081ebe9716c13b799444e805ee3f13f0fb
yifan <yifan.zhou@versa-ai.com>
2021-02-25 20:54:03 +0800

    逻辑修改

--- makaron_interface_one_at_time.py
+++ makaron_interface_one_at_time.py
@@ -6,6 +6,19 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 
 url = []
+url.append(f"https://vmk-dev.api.versa-ai.com//basic/material/app/queryBasicInfo?appSource=appstore&appVersion=1.3.0"
+           f"&appkey=vmkndev-ios&clientType=app&countryCode=&deviceId=d2e475d4862f623230aa98467ecd2146"
+           f"&idfa=00000000-0000-0000-0000-000000000000&lang=&mnet=&mobileType=iPhone13%2C3&mobileTypeCode=iPhone13%2C3"
+           f"&osType=iOS&osVersion=14.1&pointx=&pointy=&sign=9C7EE9043DBEA9364FF98F915E8F1D0F&timestamp=1614136197031")
+url.append(f"https://vmk-dev.api.versa-ai.com/guoman/material/app/titleList?appSource=appstore&appVersion=1.3.0"
+           f"&appkey=vmkndev-ios&clientType=app&countryCode=&deviceId=d2e475d4862f623230aa98467ecd2146"
+           f"&idfa=00000000-0000-0000-0000-000000000000&lang=&mnet=&mobileType=iPhone13%2C3&mobileTypeCode=iPhone13%2C3"
+           f"&osType=iOS&osVersion=14.1&pointx=&pointy=&sign=28831D292AD471A0DDF315DA165E6A1D&timestamp=1614136197032")
+url.append(f"https://vmk-dev.api.versa-ai.com//guoman/material/app/concurrentQueryInfo?appSource=appstore"
+           f"&appVersion=1.3.0&appkey=vmkndev-ios&clientType=app&countryCode=&deviceId=d2e475d4862f623230aa98467ecd2146"
+           f"&idfa=00000000-0000-0000-0000-000000000000&lang=&mnet=&mobileType=iPhone13%2C3&mobileTypeCode=iPhone13%2C3"
+           f"&osType=iOS&osVersion=14.1&pointx=&pointy=&sign=79D239ECA449600D4E08606D4A61112B&timestamp=1614136197317"
+           f"&titleCode=3be420c9b02445e7b2c781461cd71302")
 url.append(f"https://appdev.api.versa-ai.com/menu/editing?appSource=web&appVersion=4.5.9&appkey=test&clientType=app"
            f"&countryCode=CN&deviceId=8a1449de224a3cc0&imei=&lang=zh-cn&mobileType=SM-G9650&osType=ANDROID&osVersion=10"
            f"&sign=47904D4FC34A1B0380AACC9B11A0BFF1&timestamp=1613804392458&uid=&userToken=")
@@ -24,23 +37,10 @@ url.append(f"https://appdev.api.versa-ai.com/community/feed/recommend/templates?
            f"&appSource=web&appVersion=4.5.9&appkey=test&clientType=app&countryCode=CN&deviceId=8a1449de224a3cc0&from=1"
            f"&imei=&lang=zh-cn&mobileType=SM-G9650&osType=ANDROID&osVersion=10&sign=C677DE2D534BE2466251E3364599BB20"
            f"&tabId=459061311793057793&timestamp=1614233868270&uid=&userToken=")
-url.append(f"https://vmk-dev.api.versa-ai.com//basic/material/app/queryBasicInfo?appSource=appstore&appVersion=1.3.0"
-           f"&appkey=vmkndev-ios&clientType=app&countryCode=&deviceId=d2e475d4862f623230aa98467ecd2146"
-           f"&idfa=00000000-0000-0000-0000-000000000000&lang=&mnet=&mobileType=iPhone13%2C3&mobileTypeCode=iPhone13%2C3"
-           f"&osType=iOS&osVersion=14.1&pointx=&pointy=&sign=9C7EE9043DBEA9364FF98F915E8F1D0F&timestamp=1614136197031")
-url.append(f"https://vmk-dev.api.versa-ai.com/guoman/material/app/titleList?appSource=appstore&appVersion=1.3.0"
-           f"&appkey=vmkndev-ios&clientType=app&countryCode=&deviceId=d2e475d4862f623230aa98467ecd2146"
-           f"&idfa=00000000-0000-0000-0000-000000000000&lang=&mnet=&mobileType=iPhone13%2C3&mobileTypeCode=iPhone13%2C3"
-           f"&osType=iOS&osVersion=14.1&pointx=&pointy=&sign=28831D292AD471A0DDF315DA165E6A1D&timestamp=1614136197032")
-url.append(f"https://vmk-dev.api.versa-ai.com//guoman/material/app/concurrentQueryInfo?appSource=appstore"
-           f"&appVersion=1.3.0&appkey=vmkndev-ios&clientType=app&countryCode=&deviceId=d2e475d4862f623230aa98467ecd2146"
-           f"&idfa=00000000-0000-0000-0000-000000000000&lang=&mnet=&mobileType=iPhone13%2C3&mobileTypeCode=iPhone13%2C3"
-           f"&osType=iOS&osVersion=14.1&pointx=&pointy=&sign=79D239ECA449600D4E08606D4A61112B&timestamp=1614136197317"
-           f"&titleCode=3be420c9b02445e7b2c781461cd71302")
 
-time_to_Run = 10000  # 循环次数
+time_to_Run = 50000  # 循环次数
 success_num_requests = [0] * 1  # 成功次数
-max_thread_num = 500  # 线程数
+max_thread_num = 30  # 线程数
 defeat_num_requests = [0] * 1  # 返回值非200的次数
 avg_time_each_successful_request = [0] * 1  # 成功的请求耗时
 
@@ -108,8 +108,8 @@ def conn(response_code_is_200, response_code_not_200, request_time_if_successful
 if __name__ == '__main__':
     startTime = time.time()
     with ThreadPoolExecutor(max_workers=max_thread_num) as pool:  # 创建一个最大线程数为6的线程池，具体几个可以多试试
-        # for _ in range(time_to_Run):
-        while(True):
+        for _ in range(time_to_Run):
+        # while(True):
             # 将request提交给线程池
             pool.submit(conn, success_num_requests, defeat_num_requests, avg_time_each_successful_request)
     endTime = time.time()
